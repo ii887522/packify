b8c4175fc22c136a5cdc62e234a25c6dfc784135
Chi Min Yong <chimin.yong@g2g.com>
2020-12-13 17:52:50 +0800

    Add zip function

--- test/build.js
+++ test/build.js
@@ -1,2 +1,11 @@
 'use strict'
 
+import { options, dependencies, zip } from '../index.js'
+
+options.outDirPath = 'test/libs/'
+
+dependencies(() => {
+    zip('https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip')
+    zip('https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip')
+    zip('https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip')
+})
